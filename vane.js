08692121998085af91fe75acc7c95cc81e9ba374
31b4 <75566095+31b4@users.noreply.github.com>
2022-03-09 22:35:59 +0100

    Add files via upload

--- vane.js
+++ vane.js
@@ -1,9 +1,30 @@
-var t = new Date();
-var p = document.createElement("p")
-console.log(t.getHours(), t.getMinutes())
-if(t.getHours()>12 || t.getHours() == 12 && t.getMinutes()>=30){
-    p.innerHTML="Nincs."
-}else{
-    p.innerHTML="Van."
+//---------------------------VANEMELEGSZENYA???--------------------------------
+
+var zaras = [12,30]
+var nyitas = [7,15]
+vanE()
+
+
+
+
+function vanE(){
+    var p = document.createElement("p")
+    var t = new Date();
+    var h = t.getHours()
+    var m = t.getMinutes()
+    var dayOfWeek = t.getDay();
+    var isWeekend = (dayOfWeek === 6) || (dayOfWeek  === 3);
+    if (isWeekend){
+        p.innerHTML="nincs."
+    }
+    else if((h>zaras[0] || (h == zaras[0] && m >=zaras[1]))){
+        p.innerHTML="nincs."
+    }
+    else if(h<nyitas[0] || (h==nyitas[0] && m <nyitas[1])){
+        p.innerHTML="nincs."
+    }
+    else{
+        p.innerHTML="van."
+    }
+    document.body.appendChild(p)
 }
-document.body.appendChild(p)
