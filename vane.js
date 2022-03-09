a7759bccceaabfeba14be372e6de78492dbe3a79
31b4 <75566095+31b4@users.noreply.github.com>
2022-03-09 21:58:34 +0100

    Add files via upload

--- vane.js
+++ vane.js
@@ -1,6 +1,5 @@
 var t = new Date();
 var p = document.createElement("p")
-p.style.textAlign="center"
 console.log(t.getHours(), t.getMinutes())
 if(t.getHours()>12 || t.getHours() == 12 && t.getMinutes()>=30){
     p.innerHTML="Nincs."
