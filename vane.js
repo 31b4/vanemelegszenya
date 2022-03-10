23ec386425d42d5185b0ee4ca6ec2a556196a2d4
31b4 <75566095+31b4@users.noreply.github.com>
2022-03-10 20:17:27 +0100

    Add files via upload

--- vane.js
+++ vane.js
@@ -1,30 +1,57 @@
 //---------------------------VANEMELEGSZENYA???--------------------------------
 
-var zaras = [12,30]
+var zaras = [12,30]//hour,min
 var nyitas = [7,15]
+let start = (nyitas[0] * 60) + nyitas[1];
+let stop = (zaras[0] * 60) + zaras[1];
+var stat = false
+var t = new Date();
+var h = t.getHours()
+var m = t.getMinutes()
 vanE()
-
-
-
-
-function vanE(){
-    var p = document.createElement("p")
-    var t = new Date();
-    var h = t.getHours()
-    var m = t.getMinutes()
-    var dayOfWeek = t.getDay();
-    var isWeekend = (dayOfWeek === 6) || (dayOfWeek  === 3);
-    if (isWeekend){
-        p.innerHTML="nincs."
-    }
-    else if((h>zaras[0] || (h == zaras[0] && m >=zaras[1]))){
-        p.innerHTML="nincs."
-    }
-    else if(h<nyitas[0] || (h==nyitas[0] && m <nyitas[1])){
-        p.innerHTML="nincs."
+var secondBetweenTwoDate = CalcSeconds();
+MpKiir()
+setInterval(function(){ 
+    MpKiir()
+}, 1000);
+function MpKiir(){
+    if (stat) {
+        document.getElementById("mp").innerHTML=secondBetweenTwoDate +' mp múlva NEM kapható'
     }
     else{
-        p.innerHTML="van."
+        document.getElementById("mp").innerHTML=secondBetweenTwoDate +' mp múlva kapható'
     }
-    document.body.appendChild(p)
+    secondBetweenTwoDate-=1
+}
+
+function CalcSeconds() {
+    if(stat){//van
+        console.log(Math.abs(stop-((h*60)+m)))
+        return Math.abs(((stop-((h*60)+m))*60 )+ t.getSeconds())
+    }
+    else{
+        console.log(Math.abs(start-((h*60)+m)))
+        return Math.abs(((start-((h*60)+m))*60)+ t.getSeconds())
+    } 
+
+}
+function vanE(){
+    var p = document.createElement("p")
+    var dayOfWeek = t.getDay();
+    var txt=''
+    var isWeekend = (dayOfWeek === 6) || (dayOfWeek  === 3);
+    if (isWeekend){
+        txt="nincs."
+    }
+    else if((h>zaras[0] || (h == zaras[0] && m >=zaras[1]))){
+        txt="nincs."
+    }
+    else if(h<nyitas[0] || (h==nyitas[0] && m <nyitas[1])){
+        txt="nincs."
+    }
+    else{
+        txt="van."
+        stat =true
+    }
+    document.getElementById("van").innerHTML=txt
 }
